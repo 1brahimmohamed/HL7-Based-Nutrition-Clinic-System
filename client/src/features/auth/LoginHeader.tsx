eeaf00adf901a9476e490232a9f905180a86a54a
Ibrahim <imimazim7@gmail.com>
2024-04-25 23:26:11 +0200

    chore: login & logout pages and components

---
+++ client/src/features/auth/LoginHeader.tsx
@@ -0,0 +1,26 @@
+import {Link} from "react-router-dom";
+
+const LoginHeader = () => {
+    return (
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div>
+                <img
+                    className="h-14 w-auto"
+                    src="/images/brand/logo-full-icon-right.png"
+                    alt="MDIMA Logo"
+                />
+                <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign in to your account
+                </h2>
+                <p className="mt-2 text-sm leading-6 text-gray-500">
+                    Not a member?{' '}
+                    <Link to="/carrers" className="font-semibold text-primary-main hover:text-primary-dark">
+                        Join MDIMA's medical staff
+                    </Link>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default LoginHeader;
---
+++ client/src/features/auth/Logout.tsx
@@ -0,0 +1,17 @@
+import useSignOut from 'react-auth-kit/hooks/useSignOut';
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
+const Logout = () => {
+    const navigate = useNavigate();
+    const signOut = useSignOut();
+
+    useEffect(() => {
+        signOut();
+        navigate('/login');
+    }  , []);
+
+    return null;
+}
+
+export default Logout
