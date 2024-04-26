27d2a4567a0160e0ad217c2181446e44d5f62e0d
AmeeraMOhammed <87406476+AmeeraMOhammed@users.noreply.github.com>
2024-04-27 01:46:23 +0300

    edits

--- client/src/services/apiPatients.ts
+++ client/src/services/apiPatients.ts
@@ -3,16 +3,13 @@ import axios from "axios";
 const API_URL = `${import.meta.env.VITE_BASE_URL}/patient`;
 
 export const getAllPatients = async () => {
+  const res = await axios.get(`${API_URL}`);
 
-    const res = await axios.get(`${API_URL}`);
+  if (!res.data) {
+    throw new Error("Failed to fetch all patients");
+  }
 
-    if (!res.data) {
-        throw new Error("Failed to fetch all patients");
-    }
+  const { data } = res;
 
-    const { data } = res;
-
-    return data.data;
+  return data.data;
 };
-
-
